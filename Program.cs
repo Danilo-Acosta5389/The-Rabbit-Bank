bdaa7e3dc9333f8a870d3f924636e0aa2fb80450
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-01-25 13:38:00 +0100

    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace rabbit_bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Rabbit Bank");
+            var myUser = new BankUserModel { };
+            Console.ReadLine();
+        }
+    }
+}
